7141303f57f7f939c3c8c973af1cdf08615177c3
KimNayeonnnn <2020301008@skuniv.ac.kr>
2024-05-13 14:13:18 +0900

    0513

--- js/insert_img.js
+++ js/insert_img.js
@@ -1,3 +1,5 @@
+/* write_review.html 파일에서
+   "insert_img" 버튼 onclick에 넣을 함수  */
 function openFilePicker() {
-    document.getElementById('fileInput').click();
+    document.getElementById('fileUpload').click();
 }
